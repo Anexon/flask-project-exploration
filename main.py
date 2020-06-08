2bddb5cc161e617e455a33ce17a5405d8977ed24
Rubén Triviño Juárez <ruben@willimbu.com>
2020-06-08 22:13:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, session, redirect, url_for, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return '''
+            <p>Logged in as {}</p>
+            <p>To logout visit "/logout"</p>
+        '''.format(escape(session['username']))
+    return '''
+        <p>You are not logged. Visit "/login"</p>
+    '''
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Flask Project - Learning Purposes
+
+In this project I would use common and advance functionalities of Flask in order to explore its advantages and capabilities.
